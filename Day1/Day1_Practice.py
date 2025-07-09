f75a09807b30c69d963e416d5cdb32bb47fc6ca3
Oriana J. <154834553+orianacodes@users.noreply.github.com>
2025-07-09 00:06:14 -0400

    Add files via upload

---
+++ Day1/Day1_Practice.py
@@ -0,0 +1,21 @@
+#Oriana Jaramillo
+#07/08/2025
+
+#Day 1 Tasks
+#Write a short script that:
+#Asks the user their name
+#Asks how old they are
+#Prints a personalized message using both variables
+
+#----------------------------------------------------------------------------
+print("Welcome, user")
+user_name = input("what is your name ? ")
+age = input("How old are you ? ")
+print ("Welcome,", user_name, "you are", age, "years old!")
+
+#-----------------------------------------------------------------------------
+# ------------------------------ Example Output ------------------------------
+# Welcome, user
+# what is your name ? Oriana
+# How old are you ? 24
+# Welcome, Oriana you are 24 years old!
