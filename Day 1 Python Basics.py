12e69ae8609ba429a1d23f42bf7f2107e34a92a5
Oriana J. <154834553+orianacodes@users.noreply.github.com>
2025-07-08 23:39:33 -0400

    Add files via upload

---
+++ Day 1 Python Basics.py
@@ -0,0 +1,38 @@
+#Oriana J.
+#Day 1:Python basics
+#07/08/2025
+
+#Objectives:
+#Learn how to print messages to the screen
+#Store data in variables
+#Take input from the user
+
+
+# ------------------------------------------------------------#
+# 1. printing to the screen
+print ("Hello, World!")
+print ("Welcome to your Python roadmap!")
+
+
+# ------------------------------------------------------------#
+#2. using variables
+name = "Ori"
+age = 24
+print ("Name: ", name)
+print ("Age: ", age)
+
+# ------------------------------------------------------------#
+#3. Taking user input
+
+user_name = input("What is your name? ")
+print("Nice to meet you, ",user_name)
+
+# ------------------------------------------------------------#
+# 4. String concatenation & Formatting (two options)
+#option 1
+print ("your name is " + user_name
+
+#option 2
+print(f"hello, {user_name}!")
+       
+
