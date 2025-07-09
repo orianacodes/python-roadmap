4467fcfc8cd15b84b4b6cb1b10db84c7b62cb1a7
Oriana J. <154834553+orianacodes@users.noreply.github.com>
2025-07-09 17:56:28 -0400

    Update Day2_Lesson.py

--- Day2/Day2_Lesson.py
+++ Day2/Day2_Lesson.py
@@ -1,1 +1,37 @@
+#Oriana J.
+#Day 2:Data Types and Basic Math Operations
+
+# Objectives:
+
+# Python data types: `int`, `float`, `str`, `bool`
+# Type casting (converting between types)
+# Basic math operations: addition (+),
+# subtraction (-), multiplication (*), division (/), modulus (%)
+
+
+#------------------------------------------------------------#
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print (f"{num1} + {num2} = {num1 + num2}") #
+print (f"{num1} - {num2} = {num1 - num2}")
+print (f"{num1} * {num2} = {num1 * num2}")
+print (f"{num1} / {num2} = {num1 / num2}")
+print (f"{num1} % {num2} = {num1 % num2}")
+
+
+
+#------------------------------------------------------------#
+#---------------------Example Output-------------------------#
+#Enter the first number: 5
+#Enter the second number: 10
+#5.0 + 10.0 = 15.0
+#5.0 - 10.0 = -5.0
+#5.0 * 10.0 = 50.0
+#5.0 / 10.0 = 0.5
+#5.0 % 10.0 = 5.0
+#------------------------------------------------------------#
+
+
+
 
