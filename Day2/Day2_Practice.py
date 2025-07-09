b7a4f62b8bc3d99dd7c147e63a87b57686a9b1a6
Oriana J. <154834553+orianacodes@users.noreply.github.com>
2025-07-09 18:14:19 -0400

    Add files via upload

---
+++ Day2/Day2_Practice.py
@@ -0,0 +1,34 @@
+#Oriana J.
+
+
+#Day 2 Tasks
+#Asks the user to enter two numbers (they can be decimals).
+#Converts the inputs into numbers (float)
+
+#Prints the result of:
+#Addition
+#Multiplication
+#Division
+#Modulus (remainder)
+
+
+#-------------------------------------------------#
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
+
+print(f"{num1} + {num2} = {num1 + num2}") #Addition
+print(f"{num1} * {num2} = {num1 * num2}") #multiplication
+print(f"{num1} / {num2} = {num1 / num2}") #division
+print(f"{num1} % {num2} = {num1 % num2}") #modulus
+
+
+#---------------Expected Output--------------------#
+#Enter a number: 30
+#Enter a number: 10
+#30.0 + 10.0 = 40.0
+#30.0 * 10.0 = 300.0
+#30.0 / 10.0 = 3.0
+#30.0 % 10.0 = 0.0
+
+
