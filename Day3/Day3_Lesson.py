8a16112337632793e796d5f84cacacf9c3a207e7
Oriana J. <154834553+orianacodes@users.noreply.github.com>
2025-07-09 19:28:35 -0400

    Add files via upload

---
+++ Day3/Day3_Lesson.py
@@ -0,0 +1,26 @@
+#Oriana J.
+
+#Day 3: Conditional Statements
+
+
+
+#------------------------------------------------------------#
+
+score = int(input("Enter your score (0-100): "))
+
+if score >= 90:
+    print ("You got an A.")
+elif score >= 80:
+    print("You got a B.")
+elif score >= 70:
+    print("You got a C.")
+elif score >= 60:
+    print("You got a D.")
+else:
+    print("You got an F.")
+
+
+#---------------------Example Output-------------------------#
+
+#Enter your score (0-100): 100
+#You got an A.
